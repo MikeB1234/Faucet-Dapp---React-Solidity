f46ce8fb3b2c3af6ab77331be85202d46bbfad5d
MikeB1234 <mbacha2345@gmail.com>
2022-03-08 15:57:10 -0500

    added onlyOwner modifier & transferOwnership function to Faucet.sol

--- contracts/Faucet.sol
+++ contracts/Faucet.sol
@@ -3,8 +3,13 @@ pragma solidity >=0.4.22 <0.9.0;
 
 contract Faucet {
     uint256 public numOfFunders;
+    address public owner;
     mapping(address => bool) public funders;
     mapping(uint256 => address) public lutFunders;
+
+    constructor() {
+        owner = msg.sender;
+    }
 
     receive() external payable {}
 
@@ -14,6 +19,15 @@ contract Faucet {
             "Cannot withdraw more than 1 eth"
         );
         _;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only owner can call this function");
+        _;
+    }
+
+    function transferOwnership(address newOwner) external onlyOwner {
+        owner = newOwner;
     }
 
     function addFunds() external payable {
