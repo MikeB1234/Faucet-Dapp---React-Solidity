bc9fddc5ffd04b52168985e4bb2bb185f77c6777
MikeB1234 <mbacha2345@gmail.com>
2022-03-09 10:25:39 -0500

    create app, connect to metamask with window.ethereum

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
-import './App.css';
+import { useEffect } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
 
-    </div>
+  useEffect(() => {
+    const loadProvider = async () => {
+      console.log(window.web3)
+    }
+    loadProvider()
+  }, [])
+
+  return (
+    <>
+      <div className="faucet-wrapper">
+        <div className="faucet">
+          <div className="balance-view is-size-2">
+            Current Balance <strong>10</strong> ETH
+          </div>
+          <button onClick={async () => {
+            const accounts = await window.ethereum.request({ method: "eth_requestAccounts" })
+            console.log(accounts)
+          }} className="btn mr-2">Enable Ethereum</button>
+          <button className="btn mr-2">Donate</button>
+          <button className="btn">Withdraw</button>
+        </div>
+      </div>
+    </>
   );
 }
 
