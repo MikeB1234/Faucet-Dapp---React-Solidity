f4fa61e957206abdc5cf4055c7bb514b60b8fd35
MikeB1234 <mbacha2345@gmail.com>
2022-03-09 11:37:12 -0500

    integrate web3.js, set to state

--- src/App.js
+++ src/App.js
@@ -1,14 +1,41 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
+import Web3 from "web3";
 
 function App() {
 
+  const [web3Api, setWeb3Api] = useState({
+    provider: null,
+    web3: null
+  })
+
   useEffect(() => {
     const loadProvider = async () => {
-      console.log(window.web3)
+      let provider = null;
+
+      if (window.ethereum) {
+        provider = window.ethereum;
+        try {
+          await provider.enable();
+        } catch {
+          console.error("User denied account access!")
+        }
+      } else if (window.web3) {
+        provider = window.web3.currentProvider;
+      } else if (!process.env.production) {
+        provider = new Web3.providers.HttpProvider("http://localhost:7545")
+      }
+
+      setWeb3Api({
+        web3: new Web3(provider),
+        provider
+      })
     }
     loadProvider()
   }, [])
+
+  console.log(web3Api.web3)
+
 
   return (
     <>
@@ -17,10 +44,7 @@ function App() {
           <div className="balance-view is-size-2">
             Current Balance <strong>10</strong> ETH
           </div>
-          <button onClick={async () => {
-            const accounts = await window.ethereum.request({ method: "eth_requestAccounts" })
-            console.log(accounts)
-          }} className="btn mr-2">Enable Ethereum</button>
+
           <button className="btn mr-2">Donate</button>
           <button className="btn">Withdraw</button>
         </div>
